ba1f7073ec5d0a542248e5b28f83739a55c8316d
CaptainD54321 <161258713+CaptainD54321@users.noreply.github.com>
2024-02-25 18:32:47 -0500

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CaptainDTweaks
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CaptainDTweaks";
-        public const string PLUGIN_NAME = "CaptainDTweaks";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
