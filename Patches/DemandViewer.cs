1ed7d9ca0782d981556f8a74b0bac7daf645392b
CaptainD54321 <CaptainD54321@mailbox.org>
2024-03-06 13:47:20 -0500

    Added dirt prevention feature

---
+++ Patches/DemandViewer.cs
@@ -0,0 +1,11 @@
+using System;
+using HarmonyLib;
+using BepInEx;
+using SailwindModdingHelper;
+using UnityEngine;
+
+namespace CaptainDTweaks.Patches;
+
+internal static class DemandViewer {
+    
+}
---
+++ Patches/NoDirt.cs
@@ -0,0 +1,15 @@
+using System;
+using HarmonyLib;
+using BepInEx;
+using SailwindModdingHelper;
+using UnityEngine;
+
+namespace CaptainDTweaks.Patches;
+
+[HarmonyPatch(typeof(CleanableObject))]
+internal static class NoDirt {
+    [HarmonyPatch("ApplyDailyDirt")]
+    public static bool Prefix(CleanableObject __instance) {
+        return Plugin.noDirt.Value;
+    }
+}
