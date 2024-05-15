c9f8dd03ded33cc2f90d79f0b7504079cc433520
CaptainD54321 <CaptainD54321@mailbox.org>
2024-05-15 04:32:51 -0400

    Remove debug line

--- Fixes/AestrinCompass.cs
+++ Fixes/AestrinCompass.cs
@@ -11,7 +11,6 @@ internal static class CompassFix {
     [HarmonyPatch("OnLoad")]
     [HarmonyPostfix]
     internal static void FixRotation(ref ShipItemCompass __instance) {
-        Plugin.logger.LogInfo($"Running CompassFix for compass {__instance.gameObject.name}");
         if(__instance.gameObject.name.Contains("82 compass M")) {
             __instance.inventoryRotation = 180;
             __instance.inventoryRotationX = 270;
